8abe77a50bbb6feba21d5eae9b7cd290263f1917
fhag <gerard.fischer@outlook.com>
2020-04-10 16:43:08 +0200

    showfonts.py edited

--- showfonts.py
+++ showfonts.py
@@ -7,11 +7,14 @@ requires Python 3.6 or later
 '''
 import os
 
-path = '/usr/share/fonts/truetype'
+__version__ = '0.0.1'
+
+
+PATH = '/usr/share/fonts/truetype'
 
 if __name__ == '__main__':
-    print(os.listdir(path))
-    for folder in os.walk(path):
+    print(os.listdir(PATH))
+    for folder in os.walk(PATH):
         print(folder[0])
         for i, font in enumerate(folder[2]):
             if 'ttf' in font:
